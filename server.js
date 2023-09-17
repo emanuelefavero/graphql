915fca5d4891f3b96c2dc15d2bc5f36373453544
emanuelefavero <emafaux89@gmail.com>
2023-09-17 02:17:56 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+const app = express()
+
+app.use(
+  '/graphql',
+  expressGraphQL({
+    graphiql: true,
+  })
+)
+app.listen(4000, () => {
+  console.log('Server is running on port 4000')
+})
