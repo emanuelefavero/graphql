bd12376c12dce9725dd4472e1168e0317c1b1169
emanuelefavero <emafaux89@gmail.com>
2023-09-19 08:13:42 +0200

    create apollo client query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,33 @@
 import './App.css'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { useQuery, gql } from '@apollo/client'
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
-})
+// Define a GraphQL query
+const GET_BOOKS = gql`
+  query {
+    books {
+      name
+    }
+  }
+`
 
 function App() {
+  // Use the useQuery hook to make GraphQL queries from within the component
+  const { loading, error, data } = useQuery(GET_BOOKS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error {error.message}</p>
+
+  const books = data.books
+
   return (
-    <ApolloProvider client={client}>
+    <>
       <h1>GraphQL Tutorial</h1>
-    </ApolloProvider>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>{book.name}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
