5519d06873e265bcdee13802b09b5d8f615a140b
emanuelefavero <emafaux89@gmail.com>
2023-09-19 07:56:13 +0200

    connect graphql apollo client to react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import './App.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+})
 
 function App() {
-  return <>Hello</>
+  return (
+    <ApolloProvider client={client}>
+      <h1>GraphQL Tutorial</h1>
+    </ApolloProvider>
+  )
 }
 
 export default App
